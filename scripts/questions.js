01454e0f0991e52c95aa4778a7b7a1d6bfc014cd
Sax <71991536+Saxboyy@users.noreply.github.com>
2024-04-30 14:11:49 +0200

    Add historyIndex + Refactor

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,15 +1,28 @@
-import { rCountry } from "./Random.js"
+import { generateRandom, rCountry } from "./Random.js"
 const box = document.getElementById("box")
+
+const indexHistory = new Set()
 
 export async function queetion(rCountry) {
   try {
     const response = await fetch("../data/perCountry/" + rCountry + ".json")
     const data = await response.json()
-    console.log(data)
     const curiosidades = data.curiosidades
-    const randomIndex = Math.floor(Math.random() * curiosidades.length)
-    const randomCuriosidad = curiosidades[randomIndex]
-    box.innerHTML = randomCuriosidad
+
+    if (indexHistory.size === curiosidades.length) {
+      console.log("Reiniciando historial ...")
+      indexHistory.clear()
+    }
+
+    let randomIndex
+
+    do {
+      randomIndex = Math.floor(Math.random() * curiosidades.length)
+    } while (indexHistory.has(randomIndex))
+
+    const generatedQuestion = curiosidades[randomIndex]
+    box.innerHTML = generatedQuestion
+    indexHistory.add(randomIndex)
   } catch (error) {
     console.error("Error:", error)
   }
